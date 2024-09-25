49c13eb2f18d9921d8ac000bd13bd66035a74f2d
Krishnapriya Venkatakrishnan <144596138+krishnapriya-venkatakrishnan@users.noreply.github.com>
2024-09-25 10:27:17 +0200

    Alignment changes
    Alignment changes

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -27,7 +27,7 @@ const Landing = () => {
             variants={container(0.5)}
             initial="hidden"
             animate="visible"
-            className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent text-center xs:text-2xl'>
+            className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent xs:text-2xl'>
                 Frontend developer
             </motion.span>
 
